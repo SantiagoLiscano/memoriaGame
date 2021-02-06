ab61919cfa696b8de95451d1ef20f297de2ad76b
Santiago Rodriguez <santiagoliscano@gmail.com>
2021-02-06 14:12:30 -0400

    optimization images

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 var cards = document.getElementsByClassName('card');
 var backCards = document.querySelectorAll('.card > .side:nth-child(2)');
 var infoArriba = document.getElementsByTagName('h3');
-var backgrounds = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "15.png", "12.png", "14.png", "13.png", "11.png", "1.png", "2.png", "6.png", "4.png", "5.png", "3.png", "7.png", "8.png", "9.png", "11.png", "10.png", "12.png", "15.png", "14.png", "13.png"]
+var backgrounds = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "15.jpg", "12.jpg", "14.jpg", "13.jpg", "11.jpg", "1.jpg", "2.jpg", "6.jpg", "4.jpg", "5.jpg", "3.jpg", "7.jpg", "8.jpg", "9.jpg", "11.jpg", "10.jpg", "12.jpg", "15.jpg", "14.jpg", "13.jpg"]
 var cartasArriba = 0;
 var firstCard;
 var secondCard;
